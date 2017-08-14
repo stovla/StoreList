8056df965347f4da9e9194e7d849c0acbbeea996
Vlastimir Radojevic <vlastimirsd@gmail.com>
2017-08-14 17:57:49 +0200

    rearange the files

--- StoreList/DetailsVC.swift
+++ StoreList/DetailsVC.swift
@@ -108,17 +108,5 @@ class DetailsVC: UIViewController {
 //        mkMap.addAnnotation(annotation)
 //        mkMap.setRegion(location, animated: true)
 //    }
-    
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
